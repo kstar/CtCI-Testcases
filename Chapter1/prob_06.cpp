b834936b6de26aa483a7d7fbd775a25d5fc91e53
Akarsh Simha <akarsh@kde.org>
2017-01-07 02:45:40 -0800

    Manual testing for 1.6

---
+++ Chapter1/prob_06.cpp
@@ -0,0 +1,35 @@
+/***************************************************************************
+                              prob_06.cpp  -
+                             -------------------
+    begin                : Mon 10 Oct 2016 05:10:11 CDT
+    copyright            : (c) 2016 by Akarsh Simha
+    email                : akarsh.simha@kdemail.net
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+
+/* Project Includes */
+
+/* STL Includes */
+#include <string>
+#include <iostream>
+
+std::string compress( const std::string &s ) {
+    // Your code goes here
+}
+
+
+int main() {
+    std::string str;
+    std::cin >> str;
+    std::cout << compress( str ) << std::endl;
+    std::cout << compress2( str ) << std::endl;
+}
